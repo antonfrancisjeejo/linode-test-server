e435c74f190985112c260dab981cc159946f7377
Anton Francis Jeejo <anton13jeejo@gmail.com>
2021-07-07 16:00:01 +0530

    Updated for elastic bean stalk

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Linode server App</h1>");
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT||5000, () => {
   console.log("App listening on port 5000");
 });
+
+module.exports = app;
