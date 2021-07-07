7b1535ed8d3c60f542e7a1257a1a7078b5d12ff5
Anton Francis Jeejo <anton13jeejo@gmail.com>
2021-07-07 16:10:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Linode server App</h1>");
 });
 
+app.get("/new",(req,res)=>{
+  res.send("New route is working");
+});
+
 app.listen(process.env.PORT||5000, () => {
   console.log("App listening on port 5000");
 });
